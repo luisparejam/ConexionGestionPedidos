935f513d9930df519d6c94699da282031257cfbe
Luis Pareja <lpareja@hotmail.com>
2024-10-11 16:28:55 -0400

    video 92
    video 92

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.ConstrainedExecution;
 using System.Windows;
 
 namespace ConexionGestionPedidos
@@ -17,13 +19,21 @@ namespace ConexionGestionPedidos
 
             miConnexionSql = new SqlConnection(miConexion);
 
-
+            MuestraClientes();
         }
 
         private void MuestraClientes()
         {
             string consulta = "SELECT * FROM Cliente";
-
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConnexionSql);
+            using (miAdaptadorSql)
+            {
+                DataTable clientesTabla = new DataTable();
+                miAdaptadorSql.Fill(clientesTabla);
+                listaClientes.DisplayMemberPath = "nombre";
+                listaClientes.SelectedValuePath = "Id";
+                listaClientes.ItemsSource = clientesTabla.DefaultView;
+            }
         }
 
         SqlConnection miConnexionSql;
